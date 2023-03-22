6b310012ac5e18554a482de91c167ec71a5f9755
scottalyons@gmail.com <scottalyons@gmail.com>
2023-03-22 20:46:54 +0000

    Changing raw ingest path

--- hello_project/pipelines/IngestData/code/ingestdata/graph/Users_Raw.py
+++ hello_project/pipelines/IngestData/code/ingestdata/graph/Users_Raw.py
@@ -6,4 +6,4 @@ from ingestdata.config.ConfigStore import *
 from ingestdata.udfs.UDFs import *
 
 def Users_Raw(spark: SparkSession, in0: DataFrame):
-    in0.write.format("json").mode("overwrite").save(f"dbfs:/FileStore/Prophecy/hello_cicd/{Config.env}/raw/users.json")
+    in0.write.format("json").mode("overwrite").save("dbfs:/FileStore/Prophecy/hello_cicd/raw/users.json")
